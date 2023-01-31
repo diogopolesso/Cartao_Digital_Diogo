916aeac6628268af1ffd2dbce63c079ac66aa6c7
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-31 20:47:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const botmobile = document.getElementsById('bot-mobile');
+const botmobile = document.getElementById('bot-mobile');
 
-
-function toggleMenu() {
+function toggleMenu(event) {
+    if (event.type === 'toutchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', 'true');
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
 }
 
 botmobile.addEventListener('click', toggleMenu);
+botmobile.addEventListener('touchstart', toggleMenu);
