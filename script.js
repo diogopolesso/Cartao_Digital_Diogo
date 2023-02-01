72858f3a33f9c8f62e5203bc325c5a54944d672f
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-02-01 14:59:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function toggleMenu(event) {
     if (event.type === 'toutchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-//     const active = nav.classList.contains('active');
-//     event.currentTarget.setAttribute('aria-expanded', 'true');
-//     if (active) {
-//         event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
-//     } else {
-//         event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
-//     }
+    // const active = nav.classList.contains('active');
+    // event.currentTarget.setAttribute('aria-expanded', 'true');
+    // if (active) {
+    //     event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    // } else {
+    //     event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    // }
 }
 
 botmobile.addEventListener('click', toggleMenu);
